164abc9b8880321e5c7c89f83f94decb6456dbb0
maryam <103657696+yaghoobiii@users.noreply.github.com>
2022-05-19 09:46:52 +0430

    Add files via upload

---
+++ LEARNING PYTHON MARYAM YAGHOOBI (LIST).py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+
+L= ["malarduniversity ", 607 , " clss2005" , 789.009]
+L[0]
+
+
+# In[3]:
+
+
+
+L= ["malarduniversity ", 607 ,['CLASS',18], " clss2005" , 789.009]
+L[2][1]
+
+
+# In[4]:
+
+
+
+L= ["malarduniversity ", 607 , " clss2005" , 789.009]
+L[1:4]
+
+
+# In[8]:
+
+
+L= ["malarduniversity ", 607 , " clss2005" , 789.009]
+L[1:1]
+
+
+# In[11]:
+
+
+L= ["malarduniversity ", 607 , " clss2005" , 789.009]
+L[1] = "KARAJ"
+L[1] 
+"KARAJ"
+
+
+# In[15]:
+
+
+L =[1,2,3,4]
+L[1:0]
+
+
+# In[16]:
+
+
+L=[1,2,3,4,5]
+L[0:3]
+
+
+# In[ ]:
+
+
+
+

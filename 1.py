e665e6aa40e99f40549b330296d81a24e5ccef8b
maryam <103657696+yaghoobiii@users.noreply.github.com>
2023-05-01 03:02:26 +0330

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,62 @@
+# list_name = ['ali', 'hassan', 'zahra']
+# avrag_name = [12, 15, 16, 17, 20, 60]
+# list_student = []
+# if len(list_name) == len(avrag_name):
+#     x = len(list_name)
+# elif len(list_name) > len(avrag_name):
+#     x = len(list_name)
+#
+# # else:
+# #     x = len(list_name)
+# #
+# # # print(x)
+# # for i in range(0, x):
+# #     list_temp = []
+# #     list_temp.append(list_name)
+# #     list_temp.append(avrag_name)
+# #     list_student.append(list_temp)
+# #
+# #
+# #
+# # print(list_temp)
+#
+# lst_stu = []
+# for i in range(0,5):
+#     list_temp = [ ]
+#     name = input('enter your name : ')
+#     lastname = input('enter your lastname : ')
+#     list_temp.append(name)
+#     list_temp.append(lastname)
+#     s =0
+#     for j in range(0,3):
+#         number = float(input('enter your number : '))
+#         s+=number
+#         list_temp.append(number)
+#     list_temp.append(s/3)
+#     lst_stu.append(list_temp)
+# print(lst_stu)
+# max_avge =lst_stu [0][5]
+# for i in lst_stu:
+#     if i[5]>max_avge:
+#         max_avge = i[5]
+#         max_na = i[0]
+#         max_las = i[1]
+#
+# print(f'max-avge = {max_avge} max-na = {max_na} , max_las = {max_las} ')
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+
+
+
+
